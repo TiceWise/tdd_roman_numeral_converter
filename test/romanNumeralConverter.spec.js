6a4cf90d4c6abf520c89f05c79a23157c3075a43
TiceWise <thijsdgroot@gmail.com>
2021-09-09 19:59:45 +0200

    up to 400 working... but not recursive

--- test/romanNumeralConverter.spec.js
+++ test/romanNumeralConverter.spec.js
@@ -13,6 +13,18 @@ describe('Roman Number Converter', () => {
     ${8} | ${'VIII'}
     ${9} | ${'IX'}
     ${10} | ${'X'}
+    ${11} | ${'XI'}
+    ${14} | ${'XIV'}
+    ${20} | ${'XX'}
+    ${39} | ${'XXXIX'}
+    ${40} | ${'XL'}
+    ${49} | ${'XLIX'}
+    ${50} | ${'L'}
+    ${89} | ${'LXXXIX'}
+    ${90} | ${'XC'}
+    ${100} | ${'C'}
+    ${399} | ${'CCCXCIX'}
+    ${400} | ${'CD'}
     `('converts $number to $expected', ({number, expected}) => {
         expect(convertToRomanNumber(number)).toBe(expected)
     })
