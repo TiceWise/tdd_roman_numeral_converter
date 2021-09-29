a49e59cd6ebb5474d07c0a7c67c8ea76092588b8
TiceWise <thijsdgroot@gmail.com>
2021-09-29 14:17:27 +0200

    session 2 finished

--- convertToRomanNumber.js
+++ convertToRomanNumber.js
@@ -1,52 +1,28 @@
-function convertToRomanNumber(number) {
-    // const numeric = [1, 5, 10, 50, 100]
-    // const roman = ['I', 'V', 'X', 'L']
-
-    if (number === 0) {
-        return ''
+function romanNumeralConverter({remainder, numeral}, higherNumber, lowerNumber, higherNumeral, lowerNumeral) {
+    while (remainder >= higherNumber) {
+        numeral = numeral.concat(higherNumeral);
+        remainder -= higherNumber;
     }
-    if (number >= 1 && number < 5-1) {
-        return 'I'.repeat(number)
+    if (remainder >= higherNumber - lowerNumber) {
+        numeral = numeral.concat(lowerNumeral + higherNumeral);
+        remainder -= (higherNumber - lowerNumber);
     }
-    if (number >= 5-1 && number < 5) {
-        return 'IV'
-    }
-    if (number >= 5 && number < 10-1) {
-        return 'V' + convertToRomanNumber(number % 5)
-    }
-    if (number >= 10-1 && number < 10) {
-        return 'IX'
-    }
-    if (number >= 10 && number < 50-10) {
-        return 'X'.repeat(Math.floor(number / 10)) + convertToRomanNumber(number % 10)
-    }
-    if (number >= 50-10 && number < 50) {
-        return 'XL' + convertToRomanNumber(number % 10)
-    }
-    if (number >= 50 && number < 100-10) {
-        return 'L' + convertToRomanNumber(number % 50)
-    }
-    if (number >= 100-10 && number < 100) {
-        return 'XC' + convertToRomanNumber(number % 10)
-    }
-    if (number >= 100 && number < 500-100) {
-        return 'C'.repeat(Math.floor(number/100)) +convertToRomanNumber(number % 100)
-    }
-    if (number >= 500-100 && number < 500) {
-        return 'CD' + convertToRomanNumber(number % 100)
-    }
-
-    // 1 , 5 , 10 , 50 , 100 , 500 , 1000
-    // I , V , X  , L  , C   , D   , M
+    return {remainder, numeral};
 }
 
-// function convert2(number){
-//     let out = ''
-//     out = out + 'M'.repeat(Math.floor(number/1000))
-//     let remainder = number % 1000
-//     1000-100
-//     const remainder
-// }
+function convertToRomanNumber(number) {
+    let numeral = '';
+    let remainder = number;
+    let remNumTup = {remainder, numeral};
+    remNumTup = romanNumeralConverter(remNumTup, 1000, 100, "M", "C");
+    remNumTup = romanNumeralConverter(remNumTup, 500, 100, "D", "C");
+    remNumTup = romanNumeralConverter(remNumTup, 100, 10, "C", "X");
+    remNumTup = romanNumeralConverter(remNumTup, 50, 10, "L", "X");
+    remNumTup = romanNumeralConverter(remNumTup, 10, 1, "X", "I");
+    remNumTup = romanNumeralConverter(remNumTup, 5, 1, "V", "I");
+    remNumTup = romanNumeralConverter(remNumTup, 1, 0, "I", "");
 
+    return remNumTup.numeral;
+}
 
 module.exports = convertToRomanNumber
--- test/romanNumeralConverter.spec.js
+++ test/romanNumeralConverter.spec.js
@@ -9,22 +9,24 @@ describe('Roman Number Converter', () => {
     ${4} | ${'IV'}
     ${5} | ${'V'}
     ${6} | ${'VI'}
-    ${7} | ${'VII'}
-    ${8} | ${'VIII'}
     ${9} | ${'IX'}
     ${10} | ${'X'}
     ${11} | ${'XI'}
     ${14} | ${'XIV'}
+    ${19} | ${'XIX'}
     ${20} | ${'XX'}
     ${39} | ${'XXXIX'}
     ${40} | ${'XL'}
-    ${49} | ${'XLIX'}
     ${50} | ${'L'}
-    ${89} | ${'LXXXIX'}
+    ${80} | ${'LXXX'}
     ${90} | ${'XC'}
-    ${100} | ${'C'}
-    ${399} | ${'CCCXCIX'}
+    ${99} | ${'XCIX'}
+    ${300} | ${'CCC'}
     ${400} | ${'CD'}
+    ${900} | ${'CM'}
+    ${1000} | ${'M'}
+    ${1900} | ${'MCM'}
+    ${3999} | ${'MMMCMXCIX'}
     `('converts $number to $expected', ({number, expected}) => {
         expect(convertToRomanNumber(number)).toBe(expected)
     })
